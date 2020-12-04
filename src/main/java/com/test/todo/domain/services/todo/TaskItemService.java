cce9c67b0957f9213aa0cd9007520881cbc896a8
Juan Camilo Kremer Alvarez <jkremera@psl.com.co>
2020-12-04 17:39:47 -0500

    Refactoring

--- src/main/java/com/test/todo/domain/services/todo/TaskItemService.java
+++ src/main/java/com/test/todo/domain/services/todo/TaskItemService.java
@@ -1,41 +1,75 @@
 package com.test.todo.domain.services.todo;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.test.todo.adapters.mariadb.todo.entities.TaskEntity;
+import com.test.todo.adapters.mariadb.todo.entities.TaskItemEntity;
+import com.test.todo.adapters.mariadb.todo.repository.TaskItemRepository;
+import com.test.todo.domain.exceptions.NotFoundException;
 import com.test.todo.domain.models.todo.Task;
 import com.test.todo.domain.models.todo.TaskItem;
-import com.test.todo.domain.ports.todo.TaskItemPersistence;
 
 @Service
 public class TaskItemService {
 
 	@Autowired
-	private TaskItemPersistence taskItemPersistence;
+	private TaskItemRepository taskItemRepository;
 	
 	public Stream<TaskItem> findByTaskId(Task task) {        
-        return taskItemPersistence.findAllByTaskId(task);
+List<TaskItem> taskItems = new ArrayList<TaskItem>();
+		
+		taskItemRepository.findByTask(new TaskEntity(task))
+			.forEach(taskEntity -> taskItems.add(taskEntity.toTaskItem()));
+
+		return taskItems.stream();
 	}
 	
 	public TaskItem findById(Integer id) {		
-		return taskItemPersistence.findById(id);
+		return taskItemRepository.findById(id)
+				.orElseThrow(() -> new NotFoundException("Could not fine the task item: " + id))
+				.toTaskItem();
 	}
 
 	public TaskItem create(Task task, TaskItem taskItem) {
-		return taskItemPersistence.create(task, taskItem);
+		TaskItemEntity taskItemEntity = new TaskItemEntity(taskItem);
+		taskItemEntity.setCreateDate(new Date());
+		taskItemEntity.setTask(new TaskEntity(task));
+		
+		return taskItemRepository.save(taskItemEntity).toTaskItem();
 	}
 	
 	public TaskItem updateName(Integer id, TaskItem taskItem) {
-		return taskItemPersistence.updateName(id, taskItem);
+		return taskItemRepository.findById(id)
+				.map(currentTaskItem -> {
+					currentTaskItem.setName(taskItem.getName());
+					return taskItemRepository.save(currentTaskItem);
+				})
+				.orElseThrow(() -> new NotFoundException("Could not fine the task item: " + id))
+				.toTaskItem();
 	}
 	
 	public TaskItem updateStatus(Integer id) {
-		return taskItemPersistence.updateStatus(id);
+		return taskItemRepository.findById(id)
+				.map(currentTaskItem -> {
+					currentTaskItem.setFinish(!currentTaskItem.isFinish());
+					if(currentTaskItem.isFinish()) {
+						currentTaskItem.setFinishDate(new Date());
+					} else {
+						currentTaskItem.setFinishDate(null);
+					}
+					return taskItemRepository.save(currentTaskItem);
+				})
+				.orElseThrow(() -> new NotFoundException("Could not fine the task item: " + id))
+				.toTaskItem();
 	}
 	
 	public void delete(Integer id) {
-		taskItemPersistence.delete(id);
+		taskItemRepository.deleteById(id);
 	}
 }
--- src/main/java/com/test/todo/domain/services/todo/TaskService.java
+++ src/main/java/com/test/todo/domain/services/todo/TaskService.java
@@ -1,36 +1,55 @@
 package com.test.todo.domain.services.todo;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.test.todo.adapters.mariadb.todo.entities.TaskEntity;
+import com.test.todo.adapters.mariadb.todo.repository.TaskRepository;
+import com.test.todo.domain.exceptions.NotFoundException;
 import com.test.todo.domain.models.todo.Task;
-import com.test.todo.domain.ports.todo.TaskPersistence;
 
 @Service
 public class TaskService {
 
 	@Autowired
-	private TaskPersistence taskPersistence;
+	private TaskRepository taskRepository;
 
 	public Stream<Task> findAll() {
-		return taskPersistence.findAll();
+		List<Task> tasks = new ArrayList<Task>();
+		
+		taskRepository.findAll().forEach(taskEntity -> tasks.add(taskEntity.toTask()));
+
+		return tasks.stream();
 	}
 
 	public Task findById(Integer taskId) {
-		return taskPersistence.findById(taskId);
+		TaskEntity taskEntity = taskRepository.findById(taskId)
+				.orElseThrow(() -> new NotFoundException("Could not fine the task: " + taskId));
+
+		return taskEntity.toTask();
 	}
 
 	public Task create(Task task) {
-		return taskPersistence.create(task);
+		task.setCreateDate(new Date());
+
+		return taskRepository.save(new TaskEntity(task)).toTask();
 	}
 	
 	public Task update(Integer id, Task task) {
-		return taskPersistence.update(id, task);
+		TaskEntity taskEntity = taskRepository.findById(id)
+				.orElseThrow(() -> new NotFoundException("Could not fine the task: " + id));
+
+		taskEntity.setName(task.getName());
+
+		return taskRepository.save(taskEntity).toTask();
 	}
 	
 	public void delete(Integer id) {
-		taskPersistence.delete(id);
+		taskRepository.deleteById(id);
 	}
 }
